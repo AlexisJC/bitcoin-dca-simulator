1de330d937192bc85bfa5c8b26103e18dfd23fff
Alexis JC <alexisjc.dev@gmail.com>
2021-03-07 16:50:30 +0100

    feat: add inputs

--- src/index.js
+++ src/index.js
@@ -1,46 +1,16 @@
 import "./styles.css";
 import { getData } from "./api.js";
-import Chart from "chart.js";
-getData().then((dataObject) => {
-  // transformer en dataset en separant labal et données
-  const dates = [];
-  const prices = [];
-  const colors = [];
+import { generateViz } from "./dataviz";
 
-  for (const key in dataObject) {
-    const isUp = dataObject[key] > prices[prices.length - 1];
-    dates.push(key);
-    prices.push(dataObject[key]);
-    colors.push(isUp ? "green" : "red");
-  }
-  console.log(prices);
-  // coloriser en vert quand ça monte, en rouge quand ça descend
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
 
-  let ctx = document.getElementById("myChart").getContext("2d");
-  let myChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: dates,
-      datasets: [
-        {
-          label: "Bitcoin",
-          data: prices,
-          backgroundColor: colors,
-          borderColor: colors,
-          borderWidth: 1
-        }
-      ]
-    },
-    options: {
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true
-            }
-          }
-        ]
-      }
-    }
-  });
-});
+const init = async () => {
+  const dataObject = await getData(startInput.value, endInput.value);
+  generateViz(dataObject);
+};
+
+init();
+
+startInput.onchange = (e) => init();
+endInput.onchange = (e) => init();
